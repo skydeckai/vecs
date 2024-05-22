627bef27a3d0bbfdb91dee7a4290b0a26deddead
ea-open-source <open-source@eastagile.com>
2024-05-22 12:04:30 +0700

    Fix failed test

--- src/tests/test_client.py
+++ src/tests/test_client.py
@@ -7,5 +7,5 @@ def test_create_collection(client: vecs_new.Client) -> None:
     with pytest.warns(DeprecationWarning):
         client.create_collection(name="docs", dimension=384)
 
-        with pytest.raises(sqlalchemy.exc.InvalidRequestError):
+        with pytest.raises(sqlalchemy.exc.ArgumentError):
             client.create_collection(name="docs", dimension=384)
